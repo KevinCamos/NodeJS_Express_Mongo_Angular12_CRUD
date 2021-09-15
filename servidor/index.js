39550e53a53fc536acfd0c6d0edf8cd20b73536d
Kevin Camós Soto <kevincamossoto@gmail.com>
2021-09-15 16:18:30 +0200

    ng new 'Cliente' Angular 12

--- servidor/index.js
+++ servidor/index.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const conectarDB = require('./config/db');
-const cors = require("cors");
+const express = require("express");
+const conectarDB = require("./config/db");
+const cors = require("cors"); //http://www.vidamrr.com/2020/01/que-es-cors-y-como-usarlo-en-nodejs.html
 
 const app = express();
 
 conectarDB();
 
 //app.set ('port', process.env.PORT || 4000)
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
-app.use('/api/products', require('./routes/product'));
+app.use("/api/products", require("./routes/product.router"));
 
-app.listen( port, '0.0.0.0', () => { //app.get('port')
-    console.log(`El servidor está corriendo perfectamente en el puerto ${port}`)
-})
+app.listen(port, "0.0.0.0", () => {
+  //app.get('port')
+  console.log(`El servidor está corriendo perfectamente en el puerto ${port}`);
+});
--- servidor/routes/product.router.js
+++ servidor/routes/product.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/productController');
+const productController = require('../controllers/product.Controller');
 
 //api/products
 router.post('/', productController.crearProduct);
