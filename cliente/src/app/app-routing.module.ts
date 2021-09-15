38b063e28eae62d67d3fef4085a2002a9546c12b
Kevin Camós Soto <kevincamossoto@gmail.com>
2021-09-15 23:13:27 +0200

    routing activado y componentes pages creados (Home, Products, Error)

--- cliente/src/app/app-routing.module.ts
+++ cliente/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: HomeComponent }, //al crear una página como componente "ng generate component pages/home", hay que añadirlo aquí!!!! IMPORTANTE!
+  { path: 'home', component: HomeComponent }, //al crear una página como componente "ng generate component pages/home", hay que añadirlo aquí!!!! IMPORTANTE!
+  { path: 'products', component: ProductsComponent }, //al crear una página como componente "ng generate component pages/home", hay que añadirlo aquí!!!! IMPORTANTE!
+  { path: '404', component: NotFoundComponent }, //El asterisco es la página "default" si no existe la ruta, se coloca al final"
+  { path: '**', component: NotFoundComponent }, //El asterisco es la página "default" si no existe la ruta, se coloca al final"
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [//declaración de componentes
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    ProductsComponent,
   ],
-  imports: [
+  imports: [ //modulos que importamos al proyecto
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//servicios
+  bootstrap: [AppComponent]//componentes que se van a iniciar
+
 })
 export class AppModule { }
